47b7d278488d7f4f2051581ff8ae9fdc8323332a
Clayton Turner <clayton.turner@sproutsocial.com>
2022-08-08 19:05:00 -0500

    Create Card class

---
+++ Card.py
@@ -0,0 +1,20 @@
+from constants import (
+    VALID_SUITS,
+    VALID_VALUES,
+)
+
+
+class Card(object):
+    suit = str()
+    value = str()
+
+    def __init__(self, value: str, suit: str):
+        if not isinstance(suit, str) or suit.lower() not in VALID_SUITS:
+            raise Exception("Not a valid suit. Must be one of: %s" % " | ".join(list(VALID_SUITS)))
+        if not isinstance(value, str) or value.lower() not in VALID_VALUES:
+            raise Exception("Not a valid value Must be one of: %s" % " | ".join(list(VALID_VALUES)))
+        self.suit = suit.capitalize()
+        self.value = value.capitalize()
+
+    def __str__(self):
+        return "%s of %s" % (self.value, self.suit)
---
+++ constants.py
@@ -0,0 +1,24 @@
+VALID_SUITS = {
+    "clubs",
+    "diamonds",
+    "hearts",
+    "spades",
+}
+
+VALID_VALUES = {
+    "ace",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+    "joker",
+}
